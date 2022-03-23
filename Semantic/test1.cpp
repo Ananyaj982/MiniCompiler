518392d36bd98400abb127e1b02161f71db1625b
Ananyaj982 <75300517+Ananyaj982@users.noreply.github.com>
2022-03-23 11:48:01 +0530

    Create test1.cpp

---
+++ Semantic/test1.cpp
@@ -0,0 +1,25 @@
+#include"2.h"
+#include<iostream>
+#include"1.h"
+int b;
+void func(int x)
+{
+int f=100.89;
+float h=8;
+int k=90;
+int j= f+(h+k);
+if(x>30)
+{x=20;}
+}
+int main()
+
+{
+//hello obj;
+//obj.hj;
+if(b>-9)
+c=90;
+int triam=1677727;
+char siv;
+char a = 'c';
+}
+
