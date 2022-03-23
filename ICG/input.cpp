c4ef6a4035a8dfdd91b27bf9f3c6e2651d29ed74
Ananyaj982 <75300517+Ananyaj982@users.noreply.github.com>
2022-03-23 11:19:51 +0530

    Create input.cpp

---
+++ ICG/input.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include"2.h"
+#include"xyz.h"
+using namespace std;
+void main()
+{
+	int a=2;
+	float b=2;
+	if(b==3){
+	b=3.5;
+	}
+	else{
+	b=5.6;
+	}
+    b=0;
+    a=a+1;
+    b=a*b;
+    a=a*a;
+    b=1+1-3/4;   
+	
+}
