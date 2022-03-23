0acd1864db1890bb885f0f706d086c90f6bdf9fa
Ananyaj982 <75300517+Ananyaj982@users.noreply.github.com>
2022-03-23 11:51:20 +0530

    Create test1.cpp

---
+++ Phase1/test1.cpp
@@ -0,0 +1,44 @@
+// this file contains all the test cases we need in accordance with the guidelines
+
+#include"2.h"
+#include<iostream>
+#include"1.h"
+class class1
+{
+private:
+   int yu;
+
+   float hj;
+};
+int b;
+void main()
+
+{
+
+int a;
+int fg =  12233;
+float x=90.99;
+char s='a';
+a=10;
+int arr[10];
+int abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz;
+//cout<<"Value of c is"<<c;
+int b=23;
+if(b<25)
+{
+     int k=12;
+     }
+if(a>12)
+{
+      int j=90;
+      }
+else
+{
+      int g=15;
+      }
+while(k<13)
+{
+k=14;
+}
+
+}
